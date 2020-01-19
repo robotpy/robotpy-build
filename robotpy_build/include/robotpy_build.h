a72bc04ea83d46eff83476b6b367d7628f54652f
Dustin Spicuzza <dustin@virtualroadside.com>
2020-01-18 21:14:56 -0500

    Generate a better error message when pure virtuals are called
    - We can do better, but good enough for now

--- robotpy_build/include/robotpy_build.h
+++ robotpy_build/include/robotpy_build.h
@@ -11,3 +11,7 @@ typedef py::call_guard<py::gil_scoped_release> release_gil;
 
 // Use this to define your module instead of PYBIND11_MODULE
 #define RPYBUILD_PYBIND11_MODULE(variable) PYBIND11_MODULE(RPYBUILD_MODULE_NAME, variable)
+
+#define RPYBUILD_OVERLOAD_PURE_NAME(pyname, ret_type, cname, name, fn, ...) \
+    PYBIND11_OVERLOAD_INT(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, __VA_ARGS__) \
+    pybind11::pybind11_fail("Tried to call pure virtual function \"" PYBIND11_STRINGIFY(pyname) "::" name "\"");
