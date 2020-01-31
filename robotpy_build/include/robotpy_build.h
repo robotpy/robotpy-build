24232e22de0c51f86b8a41476bed436803ddee2d
Dustin Spicuzza <dustin@virtualroadside.com>
2020-01-31 18:43:46 -0500

    Make it easier to identify the source of pure virtual error messages
    - Message is now RuntimeError: <__main__.PV object at 0x7fdb3020ab48> does not override required function "PureVirtual::missingFunction"
    - Fixes #48

--- robotpy_build/include/robotpy_build.h
+++ robotpy_build/include/robotpy_build.h
@@ -12,6 +12,16 @@ typedef py::call_guard<py::gil_scoped_release> release_gil;
 // Use this to define your module instead of PYBIND11_MODULE
 #define RPYBUILD_PYBIND11_MODULE(variable) PYBIND11_MODULE(RPYBUILD_MODULE_NAME, variable)
 
-#define RPYBUILD_OVERLOAD_PURE_NAME(pyname, ret_type, cname, name, fn, ...) \
-    PYBIND11_OVERLOAD_INT(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, __VA_ARGS__) \
-    pybind11::pybind11_fail("Tried to call pure virtual function \"" PYBIND11_STRINGIFY(pyname) "::" name "\"");
+#define RPYBUILD_OVERLOAD_PURE_NAME(pyname, ret_type, cname, name, fn, ...) { \
+        PYBIND11_OVERLOAD_INT(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, __VA_ARGS__) \
+        std::string __msg("<unknown> does not override required function \"" PYBIND11_STRINGIFY(pyname) "::" name "\""); \
+        try { \
+            py::gil_scoped_acquire gil; \
+            auto obj = py::cast(static_cast<const cname *>(this)); \
+            if (obj) { \
+                __msg = std::string(py::repr(obj)) + " does not override required function \"" PYBIND11_STRINGIFY(pyname) "::" name "\""; \
+            } \
+        } catch (std::exception&) {} \
+        py::pybind11_fail(__msg); \
+    }
+
