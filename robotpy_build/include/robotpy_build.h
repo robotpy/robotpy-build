d43f35d318d2f4166aa20c5a7f628bd7994a0659
Dustin Spicuzza <dustin@virtualroadside.com>
2020-02-01 01:50:47 -0500

    Fix RPYBUILD_OVERLOAD_PURE_NAME
    - Use mechanism similar to what pybind11 uses, requires internal API
    - py::cast doesn't work for some reason, wants to copy the object implicitly or something

--- robotpy_build/include/robotpy_build.h
+++ robotpy_build/include/robotpy_build.h
@@ -12,14 +12,21 @@ typedef py::call_guard<py::gil_scoped_release> release_gil;
 // Use this to define your module instead of PYBIND11_MODULE
 #define RPYBUILD_PYBIND11_MODULE(variable) PYBIND11_MODULE(RPYBUILD_MODULE_NAME, variable)
 
+// only for use by RPYBUILD_OVERLOAD_PURE_NAME
+template <class T> py::handle __get_handle(const T *this_ptr) {
+    auto this_type = py::detail::get_type_info(typeid(T));
+    if (!this_type) return py::handle();
+    return py::detail::get_object_handle(this_ptr, this_type);
+}
+
 #define RPYBUILD_OVERLOAD_PURE_NAME(pyname, ret_type, cname, name, fn, ...) { \
         PYBIND11_OVERLOAD_INT(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, __VA_ARGS__) \
         std::string __msg("<unknown> does not override required function \"" PYBIND11_STRINGIFY(pyname) "::" name "\""); \
         try { \
             py::gil_scoped_acquire gil; \
-            auto obj = py::cast(static_cast<const cname *>(this)); \
-            if (obj) { \
-                __msg = std::string(py::repr(obj)) + " does not override required function \"" PYBIND11_STRINGIFY(pyname) "::" name "\""; \
+            auto self = __get_handle(static_cast<const cname *>(this)); \
+            if (self) { \
+                __msg = std::string(py::repr(self)) + " does not override required function \"" PYBIND11_STRINGIFY(pyname) "::" name "\""; \
             } \
         } catch (std::exception&) {} \
         py::pybind11_fail(__msg); \
