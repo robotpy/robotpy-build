c656235210502cf395c9d1d45daa317e0e31d3ee
Dustin Spicuzza <dustin@virtualroadside.com>
2020-10-17 02:48:07 -0400

    Upgrade to pybind11 v2.6.0
    - overload macros renamed
    - add compatibility macros

--- robotpy_build/include/robotpy_build.h
+++ robotpy_build/include/robotpy_build.h
@@ -12,24 +12,24 @@ typedef py::call_guard<py::gil_scoped_release> release_gil;
 // Use this to define your module instead of PYBIND11_MODULE
 #define RPYBUILD_PYBIND11_MODULE(variable) PYBIND11_MODULE(RPYBUILD_MODULE_NAME, variable)
 
-// only for use by RPYBUILD_OVERLOAD_PURE_POST_INT
+// only for use by RPYBUILD_OVERRIDE_PURE_POST_IMPL
 template <class T> py::handle __get_handle(const T *this_ptr) {
     auto this_type = py::detail::get_type_info(typeid(T));
     if (!this_type) return py::handle();
     return py::detail::get_object_handle(this_ptr, this_type);
 }
 
-#define PYBIND11_OVERLOAD_CUSTOM_INT(ret_type, cname, name, ...)                          \
-    {                                                                                     \
+#define RPYBUILD_OVERRIDE_CUSTOM_IMPL(ret_type, cname, name, ...)                         \
+    do {                                                                                  \
         py::gil_scoped_acquire gil;                                                       \
-        py::function overload = py::get_overload(static_cast<const cname *>(this), name); \
-        if (overload)                                                                     \
+        py::function override = py::get_override(static_cast<const cname *>(this), name); \
+        if (override)                                                                     \
         {                                                                                 \
-            return custom_fn(overload);                                                   \
+            return custom_fn(override);                                                   \
         }                                                                                 \
-    }
+    } while (false)
 
-#define RPYBUILD_OVERLOAD_PURE_POST_INT(pyname, cname, name)                                                                           \
+#define RPYBUILD_OVERRIDE_PURE_POST_IMPL(pyname, cname, name)                                                                          \
     std::string __msg("<unknown> does not override required function \"" PYBIND11_STRINGIFY(pyname) "::" name "\"");                   \
     try                                                                                                                                \
     {                                                                                                                                  \
@@ -43,20 +43,31 @@ template <class T> py::handle __get_handle(const T *this_ptr) {
     catch (std::exception &)                                                                                                           \
     {                                                                                                                                  \
     }                                                                                                                                  \
-    py::pybind11_fail(__msg);
+    py::pybind11_fail(__msg)
 
-#define RPYBUILD_OVERLOAD_PURE_CUSTOM_NAME(pyname, ret_type, cname, name, fn, ...)                     \
-    {                                                                                                  \
-        PYBIND11_OVERLOAD_CUSTOM_INT(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, __VA_ARGS__) \
-        RPYBUILD_OVERLOAD_PURE_POST_INT(pyname, cname, name)                                           \
+#define RPYBUILD_OVERRIDE_PURE_CUSTOM_NAME(pyname, ret_type, cname, name, fn, ...)                      \
+    {                                                                                                   \
+        RPYBUILD_OVERRIDE_CUSTOM_IMPL(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, __VA_ARGS__);\
+        RPYBUILD_OVERRIDE_PURE_POST_IMPL(pyname, PYBIND11_TYPE(cname), name);                           \
     }
 
-#define RPYBUILD_OVERLOAD_PURE_NAME(pyname, ret_type, cname, name, fn, ...)                     \
-    {                                                                                           \
-        PYBIND11_OVERLOAD_INT(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, __VA_ARGS__) \
-        RPYBUILD_OVERLOAD_PURE_POST_INT(pyname, cname, name)                                    \
+#define RPYBUILD_OVERRIDE_PURE_NAME(pyname, ret_type, cname, name, fn, ...)                      \
+    {                                                                                            \
+        PYBIND11_OVERRIDE_IMPL(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, __VA_ARGS__);\
+        RPYBUILD_OVERRIDE_PURE_POST_IMPL(pyname, PYBIND11_TYPE(cname), name);                    \
     }
 
-#define RPYBUILD_OVERLOAD_CUSTOM_NAME(ret_type, cname, name, fn, ...)                              \
-    PYBIND11_OVERLOAD_CUSTOM_INT(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, __VA_ARGS__) \
+#define RPYBUILD_OVERRIDE_CUSTOM_NAME(ret_type, cname, name, fn, ...)                               \
+    RPYBUILD_OVERRIDE_CUSTOM_IMPL(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, __VA_ARGS__);\
     return cname::fn(__VA_ARGS__)
+
+//
+// backwards compat
+//
+
+#define RPYBUILD_OVERLOAD_PURE_CUSTOM_NAME(pyname, ret_type, cname, name, fn, ...) \
+    RPYBUILD_OVERRIDE_PURE_CUSTOM_NAME(pyname, PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, fn, __VA_ARGS__)
+#define RPYBUILD_OVERLOAD_CUSTOM_NAME(ret_type, cname, name, fn, ...) \
+    RPYBUILD_OVERRIDE_CUSTOM_NAME(PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, fn, __VA_ARGS__)
+#define RPYBUILD_OVERLOAD_PURE_NAME(pyname, ret_type, cname, name, fn, ...) \
+    RPYBUILD_OVERRIDE_PURE_NAME(pyname, PYBIND11_TYPE(ret_type), PYBIND11_TYPE(cname), name, fn, __VA_ARGS__)
