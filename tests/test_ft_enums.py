e0ac69f0451766dcdf12da9d3f928865ade19fd3
Dustin Spicuzza <dustin@virtualroadside.com>
2021-01-03 22:55:44 -0500

    Update enum tests

--- tests/test_ft_enums.py
+++ tests/test_ft_enums.py
@@ -9,8 +9,21 @@ def test_enums():
     assert ft.EnumContainer.InnerEnum.IE1 == 1
     assert ft.NSEnumContainer.InnerEnum.NSIE1 == 1
 
+    # Unnamed enums are hoisted as integers to their scope
+    # - not supported yet for globals
+    # assert ft._rpytest_ft.UGEX == 7
+    # assert ft._rpytest_ft.NSUGEX == 5
+    assert ft.EnumContainer.UEX == 4
+
     # enum class are specific types
     assert "GCE1" in ft.GCEnum.GCE1.__members__
     assert "NSGE2" in ft.NSGCEnum.NSGE2.__members__
     assert "IEC1" in ft.EnumContainer.InnerCEnum.__members__
     assert "NSIEC1" in ft.NSEnumContainer.InnerCEnum.__members__
+
+
+def test_enum_strip_prefix():
+    # 1 isn't a valid identifier, so it's not stripped
+    assert ft.StripPrefixEnum.STRIP_1 == 1
+    # STRIP_B is converted to B
+    assert ft.StripPrefixEnum.B == 2
