c7fd9bd4e053338d0317bd0e4b9ec496e2b1d662
Dustin Spicuzza <dustin@virtualroadside.com>
2022-09-28 01:49:07 -0400

    Always add 'template' to methods with template parameters
    - Strictly speaking, this is only needed if the class also has a template
      parameter, but it doesn't seem to hurt and this is simpler

--- tests/cpp/rpytest/ft/include/templates/fn.h
+++ tests/cpp/rpytest/ft/include/templates/fn.h
@@ -9,3 +9,13 @@ struct TClassWithFn
         return t;
     }
 };
+
+template <int I>
+struct TTClassWithFn
+{
+    template <typename T>
+    static T getT(T t)
+    {
+        return t + I;
+    }
+};
