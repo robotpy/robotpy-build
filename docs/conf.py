6de622df5b0163a0d12f50c129f5ac16dfe1c917
Dustin Spicuzza <dustin@virtualroadside.com>
2024-01-15 00:40:18 -0500

    Update RTD configuration

--- docs/conf.py
+++ docs/conf.py
@@ -66,16 +66,7 @@ intersphinx_mapping = {
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-else:
-    html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
